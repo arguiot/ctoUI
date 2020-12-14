c0d580b8dfe32288fa00348c2a62eefea5fe0848
Arthur Guiot <arguiot@gmail.com>
2020-12-14 17:35:02 -0500

    Added docs & working on dispatcher

--- src/PluginUI.ts
+++ src/PluginUI.ts
@@ -1,73 +1,99 @@
-import { Configuration, ConfigurationInitiator } from "./Configuration";
-import { Algorithm } from "./Algorithm";
-import { render, View } from "./Views/View";
+import {
+	Configuration,
+	ConfigurationInitiator
+} from "./Configuration";
+import {
+	Algorithm
+} from "./Algorithm";
+import {
+	render, ViewType,
+} from "./Views/View";
+import { NotificationCenter } from "@arguiot/broadcast.js";
 
 /**
  * Creates and manages the plugin lifecycle
  */
 
 export class PluginUI {
-  /**
-   * Configuration for the plugin. The configuration defines the views and he layout of the plugin.
-   */
-  configuration: Configuration;
-  /**
-   * The cipher algorithm object
-   */
-  algorithm: Algorithm;
-  /**
-   * Where the plugin will be rendered
-   */
-  parent: HTMLElement;
+	/**
+	 * Configuration for the plugin. The configuration defines the views and he layout of the plugin.
+	 */
+	configuration: Configuration;
+	/**
+	 * The cipher algorithm object
+	 */
+	algorithm: Algorithm;
+	/**
+	 * Where the plugin will be rendered
+	 */
+	parent: HTMLElement;
 
-  /**
-   * Initiate the plugin
-   * @param config Configuration for the plugin. The configuration defines the views and he layout of the plugin.
-   */
-  constructor(
-    algorithm: new (bindings: Configuration) => Algorithm,
-    config: ConfigurationInitiator
-  ) {
-    this.configuration = this.configure(config);
-    this.algorithm = new algorithm(this.configuration);
-    this.parent = config.parent;
-  }
+	/**
+	 * Initiate the plugin
+	 * @param config Configuration for the plugin. The configuration defines the views and he layout of the plugin.
+	 */
+	constructor(
+		algorithm: new(bindings: Configuration) => Algorithm,
+		config: ConfigurationInitiator
+	) {
+		this.configuration = this.configure(config);
+		this.algorithm = new algorithm(this.configuration);
+		this.parent = config.parent;
+		this.containers = []
 
-  configure(config: ConfigurationInitiator) {
-    return {
-      input: new config.input(),
-      output: new config.output(),
-      direction: config.direction,
-      options: Object.fromEntries(
-        Object.entries(config.options).map(entry => {
-          const view = new entry[1]();
-          return [entry[0], view];
-        })
-      ),
-    };
-  }
+		NotificationCenter.default.addObserver("requestRender", this.dispatchUpdate.bind(this))
+	}
 
-  render(windowObject: any) {
-    ["input", "output"].forEach(key => {
-      let doc: Document;
-      if (__DEV__ && typeof window != "undefined") {
-        doc = windowObject.document;
-      } else {
-        doc = window.document;
-      }
-      const container = doc.createElement("div");
-      container.className = key;
-      this.parent.appendChild(container);
+	configure(config: ConfigurationInitiator) {
+		return {
+			input: new config.input(),
+			output: new config.output(),
+			direction: config.direction,
+			options: Object.fromEntries(
+				Object.entries(config.options).map(entry => {
+					const view = new entry[1]();
+					return [entry[0], view];
+				})
+			),
+		};
+	}
 
-      interface Indexable<T = any> {
-        [key: string]: T;
-      }
+	containers: Array<HTMLElement>
 
-      const config = (this.configuration as unknown) as Indexable<View>;
-      const element = config[key].render();
-      if (typeof element != "undefined") {
-        render((element as unknown) as JSX.Element, container);
-      }
-    });
-  }
+	render(windowObject: any) {
+		const views = [
+			["input", this.configuration.input],
+			["output", this.configuration.output],
+			...Object.entries(this.configuration.options)
+		]
+		views.forEach(view => {
+			let doc: Document;
+			if (__DEV__ && typeof window != "undefined") {
+				doc = windowObject.document;
+			} else {
+				doc = window.document;
+			}
+			const container = doc.createElement("div");
+			container.className = (view[0] as string)
+			this.parent.appendChild(container);
+
+			const element = (view[1] as ViewType).render();
+			render(element, container);
+
+			this.containers.push(container)
+		});
+	}
+
+
+	dispatchUpdate() {
+		const views = [
+			this.configuration.input,
+			this.configuration.output,
+			...Object.values(this.configuration.options)
+		]
+
+		views.forEach((view, index) => {
+			view.update(this.containers[index], this.configuration)
+		})
+	}
 }
--- src/Views/View.ts
+++ src/Views/View.ts
@@ -1,6 +1,11 @@
-import { Configuration } from "../Configuration";
-import { Placement, PlacementType } from "./Placement";
-import { NotificationCenter } from "@arguiot/broadcast.js";
+import {
+	Configuration
+} from "../Configuration";
+import {
+	Placement,
+	PlacementType
+} from "./Placement";
+import { Notification, NotificationCenter } from "@arguiot/broadcast.js";
 
 /**
  * View protocol.
@@ -8,32 +13,67 @@ import { NotificationCenter } from "@arguiot/broadcast.js";
  * A View is object that translates into an HTML element, and manages its lifecycle.
  */
 export interface ViewType {
-  placement: PlacementType;
-  render(): JSX.Element | void;
-  update(element: HTMLElement, state: Configuration): void;
-  value: any;
+	placement: PlacementType;
+	render(): JSX.Element;
+	update(element: HTMLElement, state: Configuration): void;
+	value: any;
 }
 
-export class View implements ViewType {
-  placement: PlacementType;
-
-  constructor() {
-    NotificationCenter.default.addObserver(
-      "render",
-      this.render.bind(this),
-      `${NotificationCenter.default.observers.length}`
-    );
-
-    this.placement = Placement.Option();
-  }
-
-  render() {}
-  update(_element: HTMLElement, _state: Configuration) {}
-
-  get value() {
-    return null;
-  }
-  set value(_val: any) {}
+export abstract class View implements ViewType {
+	/**
+	 * Where to display the view
+	 */
+	placement: PlacementType;
+	/**
+	 * Creates a new view.
+	 * 
+	 * When extending a View, make sure to call `super`:
+	 * ```js
+	 * class MyView extends View {
+	 *	placement: PlacementType;
+	 *
+	 *	constructor() {
+	 *		super();
+	 *		this.placement = Placement.Input;
+	 *	}
+	 * ```
+	 * 
+	 * This way, you can specify a custom placement
+	 */
+	constructor() {
+		this.placement = Placement.Option();
+	}
+	/**
+	 * Method that will be called once, to create the HTML for the component
+	 */
+	abstract render(): JSX.Element
+	/**
+	 * Method that will be called when another component requires to be updated.
+	 * 
+	 * It must be very lightweight as it might be called often.
+	 * @param element The parent element where the component is.
+	 * @param state The configuration object, that acts as a state
+	 */
+	abstract update(element: HTMLElement, state: Configuration): void
+	/**
+	 * Getter that must return the internal state of the component.
+	 * 
+	 * In the case of a text input for example, `value` should return the `string` written inside the text field.
+	 */
+	abstract get value(): any
+	/**
+	 * Setter that should update the internal of the component.
+	 * 
+	 * In the case of a text input for example, `value` should set the `string` written inside the text field.
+	 */
+	abstract set value(newValue: any)
+	/**
+	 * When your view changes, ask for an update, this will update all other views impacted.
+	 */
+	requestUpdate() {
+		const request = new Notification("requestRender")
+		NotificationCenter.default.post(request)
+	}
 }
 
 /**
@@ -42,18 +82,18 @@ export class View implements ViewType {
  * @param container The HTML element you want to render your component in
  */
 export function render(element: JSX.Element, container: HTMLElement) {
-  const dom = container.ownerDocument.createElement(element.type);
-  const isProperty = (key: any) => key !== "children";
-  Object.keys(element.props)
-    .filter(isProperty)
-    .forEach(name => {
-      dom[name] = element.props[name];
-    });
+	const dom = container.ownerDocument.createElement(element.type);
+	const isProperty = (key: any) => key !== "children";
+	Object.keys(element.props)
+		.filter(isProperty)
+		.forEach(name => {
+			dom[name] = element.props[name];
+		});
 
-  if (Array.isArray(element.props.children)) {
-    element.props.children.forEach((child: JSX.Element) => render(child, dom));
-  } else if (typeof element.props.children == "string") {
-    (dom as HTMLElement).innerHTML = element.props.children;
-  }
-  container.appendChild(dom);
+	if (Array.isArray(element.props.children)) {
+		element.props.children.forEach((child: JSX.Element) => render(child, dom));
+	} else if (typeof element.props.children == "string") {
+		(dom as HTMLElement).innerHTML = element.props.children;
+	}
+	container.appendChild(dom);
 }
